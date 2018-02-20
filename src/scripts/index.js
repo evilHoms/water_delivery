3063a23872e9507a1bc5325c64129b4935697d7b
Igor Tciva <homspage@gmail.com>
2018-02-20 03:48:19 +0300

    first version

---
+++ src/scripts/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const header = document.querySelector('.main-header');
+const firstSection = document.querySelector('section');
+const hiddenBg = document.querySelector('.hidden-fixed-bg');
+
+firstSection.style.marginTop = header.getBoundingClientRect().height + 'px';
+
+document.addEventListener('scroll', onDocumentScroll);
+
+function onDocumentScroll(e) {
+  if (window.scrollY > window.innerHeight) {
+    hiddenBg.classList.remove('hidden');
+  }
+  else {
+    hiddenBg.classList.add('hidden');
+  }
+}
